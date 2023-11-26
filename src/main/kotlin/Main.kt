dd81a94ab1efb7da292253d28ffc5b6028cb0351
nolan <nolan.s.locke@gmail.com>
2023-11-26 15:20:31 -0700

    add time taken print

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,13 @@
+import java.time.Instant
+
 suspend fun main() {
+    val startTime = Instant.now().toEpochMilli()
     val generator = Generator(
         sheetID = "1bi-MqGwfqEBgKGzkezM0M2CW7xJw066YwshSpxw6uM4",
         eventID = "2023azgl"
     )
 
     generator.generate()
-    println("Done!")
+    val timeTaken = Instant.now().toEpochMilli() - startTime
+    println("Done in ${timeTaken}ms!")
 }
