d90985efa647c68d5288665662ec43fa7fc1fb9c
nolan <nolan.s.locke@gmail.com>
2023-11-26 11:46:49 -0700

    Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val iterator = args.iterator()
+    while (iterator.hasNext()) {
+        when (iterator.next()) {
+            "--test" -> {
+
+            }
+        }
+    }
+}
