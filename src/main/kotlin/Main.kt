0af1661dc92159c078486cc844f4285c5876cea1
nolan <nolan.s.locke@gmail.com>
2024-02-16 12:01:21 -0700

    update event ID

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ suspend fun main() {
     val startTime = Instant.now().toEpochMilli()
     val generator = Generator(
         sheetID = "1bi-MqGwfqEBgKGzkezM0M2CW7xJw066YwshSpxw6uM4",
-        eventID = "2023azgl"
+        eventID = "2024azva"
     )
 
     generator.generate()
