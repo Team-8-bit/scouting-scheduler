5ea820545262c7b2e7aa569f85f0e91574c9261c
nolan <nolan.s.locke@gmail.com>
2024-02-16 12:01:46 -0700

    remove pit scouting stuff

--- src/main/kotlin/Generator.kt
+++ src/main/kotlin/Generator.kt
@@ -6,21 +6,6 @@ class Generator(val sheetID: String, val eventID: String) {
         val matchData = TBA.getEventMatches(eventID).filter { it.comp_level == "qm" }.sortedBy { it.match_number }
         val matchScoutingData = getMatchScoutingData(matchData)
         googleSheet["MatchScouting!A2:H1000"] = matchScoutingData
-
-        println("Updating pit data...")
-        val pitData = TBA.getEventTeams(eventID).map { it.removePrefix("frc") }.sortedBy { it.toIntOrNull() }
-        val pitScoutingData = getPitScoutingData(pitData)
-        googleSheet["PitScouting!A2:A1000"] = pitScoutingData
-    }
-
-    private fun getPitScoutingData(teams: List<String>): MutableSheetData {
-        val data: MutableSheetData = mutableListOf()
-        teams.forEach {
-            val row = mutableListOf<String>()
-            row.add(it)
-            data.add(row)
-        }
-        return data
     }
 
     private fun getMatchScoutingData(matches: List<Match>): MutableSheetData {
